f3c23eac536be2212b6bf88d0df9020d05590fee
Ozan Halis Ilter <ozanhalisilter@gmail.com>
2021-03-30 15:28:49 +0300

    Hello There

---
+++ main.py
@@ -0,0 +1,12 @@
+from stdlib import stddraw
+
+stddraw.line(0.1, 0.3, 0.2, 0.2)
+stddraw.line(0.2, 0.2, 0.3, 0.3)
+stddraw.line(0.3, 0.3, 0.4, 0.2)
+stddraw.line(0.4, 0.2, 0.5, 0.3)
+stddraw.line(0.5, 0.3, 0.6, 0.2)
+stddraw.line(0.6, 0.2, 0.7, 0.3)
+stddraw.line(0.7, 0.3, 0.8, 0.2)
+stddraw.line(0.8, 0.2, 0.9, 0.3)
+stddraw.line(0.9, 0.3, 1, 0.2)
+stddraw.show()
