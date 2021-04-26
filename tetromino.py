8ac1ae13f32cad7f2b830d53320f794e7ab82647
Cem <cemsanli2000@gmail.com>
2021-04-26 19:48:25 +0300

    saçma commentleri sildim

--- tetromino.py
+++ tetromino.py
@@ -160,7 +160,6 @@ class Tetromino:
       return True  # successful move in the given direction
 
    def rotate_ccw(self,game_grid):
-      #array of positions (nasıl initialize edildiğini bilmiyorum bakmaya da üşendim :p)
       new_position=[0,0,0,0]
       print("----ROTATE----")
       #counter to check if all can rotate
@@ -209,7 +208,6 @@ class Tetromino:
          self.tile_matrix = np.rot90(self.tile_matrix, 3)
 
    def rotate(self,game_grid):
-      #array of positions (nasıl initialize edildiğini bilmiyorum bakmaya da üşendim :p)
       new_position=[0,0,0,0]
       print("----ROTATE----")
       #counter to check if all can rotate
@@ -254,9 +252,6 @@ class Tetromino:
       #change tile matrix if all tiles can rotate
       if counter==4:
          self.tile_matrix = np.rot90(self.tile_matrix, 3)
-# Ekstra not: muhtemelen can_rotate i
-# daha güzel yazabilirsek countera ihtiyaç duymadan da halledebiliriz ama şimdilik oldu gibi
-# daha güzel yazmaktan kastim 4 pozisyonu da aynı anda alması
 
 
    def can_rotate(self,pos,game_grid):
